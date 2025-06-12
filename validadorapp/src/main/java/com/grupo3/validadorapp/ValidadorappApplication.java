c8711e5987392741f927adfaf12beefde9805e6b
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-06-12 18:25:37 -0400

    chore: Se inicializa aplicación Spring Boot

---
+++ validadorapp/src/main/java/com/grupo3/validadorapp/ValidadorappApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.validadorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidadorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidadorappApplication.class, args);
+	}
+
+}
---
+++ validadorapp/src/test/java/com/grupo3/validadorapp/ValidadorappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.validadorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidadorappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

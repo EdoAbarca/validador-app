a1166402617d432d3dbd429cbe95930b13fe5267
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-06-12 22:31:20 -0400

    feat: Se actualiza Java principal con código ejercicio guiado

--- validadorapp/src/main/java/com/grupo3/validadorapp/ValidadorappApplication.java
+++ validadorapp/src/main/java/com/grupo3/validadorapp/ValidadorappApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ValidadorappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ValidadorappApplication.class, args);
+		String usuario = "admin";
+		String contraseña = "123456"; // Hardcoded credentials
+		if (usuario.equals("admin")) {
+			System.out.println("¡Bienvenido administrador!");
+		}
 	}
 
 }
